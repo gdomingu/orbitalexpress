5b7b7d07ffcc618e686f1db0730b93328f70b84c
Zara Peisker <zarapeisker@gmail.com>
2013-07-30 22:38:43 -0700

    flight reservation association tests

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe "Flight" do
-
+  before do
+    @reservation = FactoryGirl.create(:reservation)
+    @flight = FactoryGirl.create(:flight)
+    @seat = FactoryGirl.create(:seat)
+  end
 ##Gabe is going to do a spec test for flight and location relationship
 
-##Zara is going to do for flight and reservation
+  it "has many reservations" do
+    @fight.reservations << @reservation
+    expect(@flight.reservations).to include(@reservation)
+  end
 
+  it "has many seats" do
+    @seat.reservations << @reservation
+    expect(@seat.reservations).to include(@reservation)
+  end
 ##Alex is going to do launch window and weekday
 
 
