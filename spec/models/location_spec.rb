74d71cd8b90cfe563302a70af89a4cf2f9bf9099
Gabe <gabedominguez@outlook.com>
2013-07-31 07:58:19 -0700

    added one test for location model

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,12 +2,15 @@ require 'spec_helper'
 
 describe "Location" do
   before do
-    FactoryGirl.create(:launch_location)
-    FactoryGirl.create(:docking_location)
+    @launch_location = FactoryGirl.create(:launch_location)
+    @docking_location = FactoryGirl.create(:docking_location)
   end
 
 ##Gabe is going to do model test for location and flight
-
+  it "can be saved to a flight" do
+    @flight = Flight.create(:docking_at_id => @docking_location.id, :launch_from_id => @lauch_location.id)
+    expect(@flight.locations).to include(@launch_location)
+  end
 
 
 end
