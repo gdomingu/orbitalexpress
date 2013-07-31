b17a351dc5a7ebdba7ba9dd2ff4c9f91ac1d5cc2
Gabe <gabedominguez@outlook.com>
2013-07-30 21:29:02 -0700

    added some integration tests for sign in log in

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe "Homepage" do
+  before do
+    visit root_path
+  end
 
+  it "displays the sign up form via AJAX" do
+    click_link "Sign up"
+
+    expect(page).to have_css('form#new_venue')
+  end
+
+  it "displays the log in form via AJAX" do
+    click_link "Log in"
+
+    expect(page).to have_css('#signup_window')
+  end
 end
