fd3ed9b0cedfce59b57a1a806bda712447b4f4d6
Gabe <gabedominguez@outlook.com>
2013-07-30 20:59:27 -0700

    added relationship location flight

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,7 @@ class Flight < ActiveRecord::Base
   attr_accessible :docking_at_id, :docking_hour_id, :launch_hour_id, :launching_from_id, :name, :weekday_id
   has_many :seats, :through => :reservations
   has_many :reservations
+
+  belongs_to :launch_location, :class_name => "Location", :foreign_key => "launching_from_id"
+  belongs_to :docking_location, :class_name => "Location", :foreign_key => "docking_at_id"
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,6 @@
 class Location < ActiveRecord::Base
   attr_accessible :name, :spaceport
+
+  has_many :launched_flights, :class_name => "Flights", :foreign_key => 'launching_from_id'
+  has_many :docked_flights, :class_name => "Flights", :foreign_key => 'docking_at_id'
 end
