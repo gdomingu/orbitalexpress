0f99f116ad6411c6947c9bafc72220ddb12e2ad1
Zara Peisker <zarapeisker@gmail.com>
2013-07-30 20:31:06 -0700

    added flight model associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,3 +1,5 @@
 class Flight < ActiveRecord::Base
   attr_accessible :docking_at_id, :docking_hour_id, :launch_hour_id, :launching_from_id, :name, :weekday_id
+  has_many :seats, :through => :reservations
+  has_many :reservations
 end
