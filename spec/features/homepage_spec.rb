f86d30da78ea2d787f9f6954882ec433a53d8a94
Gabe <gabedominguez@outlook.com>
2013-07-31 00:10:40 -0700

    all specs for ajax sign up login pass woot

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Homepage" do
+describe "Homepage", js: true do
   before do
     visit root_path
   end
@@ -8,12 +8,12 @@ describe "Homepage" do
   it "displays the sign up form via AJAX" do
     click_link "Sign up"
 
-    expect(page).to have_css('form#new_venue')
+    expect(page).to have_css('form#new_user')
   end
 
   it "displays the log in form via AJAX" do
     click_link "Log in"
 
-    expect(page).to have_css('#signup_window')
+    expect(page).to have_css('form#new_user_session')
   end
 end
