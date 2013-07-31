791ae08e5c0fb4c29c767faad8b6398e8cade573
Patsy Price <patsy.m.price@gmail.com>
2013-07-30 21:33:46 -0700

    flightpage test spec

---
+++ spec/features/flightpage_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+
+describe "Flightpage" do
+  before do
+    visit root_path
+  end
+
+end
+
+describe "searching for a flight by origin spaceport" do
+
+end
+
+describe "searching for a flight by destination spaceport" do
+
+end
+
+describe "searching for a flight by flight time" do
+
+end
