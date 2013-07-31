63b995125cdef012308ec30796a062259cce28fc
Gabe <gabedominguez@outlook.com>
2013-07-30 17:54:57 -0700

    set up rails and gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orbitexpress::Application.config.secret_token = '3a81743cb2aa22d9cb6c24724d1ee176ed4e9545b391ce368bc33524b235ad6779c5a517a426b36009354a89af93d70e6c449444335a2f242979feaf4d2e51b3'
